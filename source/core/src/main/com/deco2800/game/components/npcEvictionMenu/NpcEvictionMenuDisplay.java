3d9a765183c9e9d0198676ff9bc4d73b222f8028
qiansen jin <qiansen.jin@uqconnect.edu.au>
2022-08-28 22:34:20 +0800

    Made a new confirm popup window and button for NpcEvictionMenu.

--- source/core/src/main/com/deco2800/game/components/npcEvictionMenu/NpcEvictionMenuDisplay.java
+++ source/core/src/main/com/deco2800/game/components/npcEvictionMenu/NpcEvictionMenuDisplay.java
@@ -1,17 +1,19 @@
 package com.deco2800.game.components.npcEvictionMenu;
 
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.BitmapFont;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.graphics.g2d.TextureRegion;
 import com.badlogic.gdx.scenes.scene2d.Actor;
-import com.badlogic.gdx.scenes.scene2d.ui.ImageButton;
-import com.badlogic.gdx.scenes.scene2d.ui.Label;
-import com.badlogic.gdx.scenes.scene2d.ui.Table;
-import com.badlogic.gdx.scenes.scene2d.ui.TextButton;
+import com.badlogic.gdx.scenes.scene2d.InputEvent;
+import com.badlogic.gdx.scenes.scene2d.ui.*;
 import com.badlogic.gdx.scenes.scene2d.utils.ChangeListener;
+import com.badlogic.gdx.scenes.scene2d.utils.ClickListener;
 import com.badlogic.gdx.scenes.scene2d.utils.Drawable;
 import com.badlogic.gdx.scenes.scene2d.utils.TextureRegionDrawable;
+import com.badlogic.gdx.utils.Align;
 import com.deco2800.game.GdxGame;
 import com.deco2800.game.components.settingsmenu.SettingsMenuDisplay;
 import com.deco2800.game.ui.UIComponent;
@@ -35,8 +37,86 @@ public class NpcEvictionMenuDisplay extends UIComponent {
         super.create();
         addActors();
     }
-
+    Window window;
+    Skin mskin;
+    TextButton tbOk, tbCancel;
+    Label confirmLabel;
+    Label infoLabel;
     private void addActors() {
+        mskin = new Skin(Gdx.files.internal("sk/uiskin.json"));
+        BitmapFont bitmapFont = new BitmapFont(Gdx.files.internal("flat-earth/skin/fonts/pixel_32_modal.fnt"));
+        window = new Window("", mskin);
+        // The default text is displayed on the left
+        Label.LabelStyle confirmstyle = new Label.LabelStyle();
+        confirmstyle.fontColor=new Color(66  , 39, 39, 1);
+        confirmstyle.font=bitmapFont;
+        confirmLabel = new Label("CONFIRM", confirmstyle);
+        confirmLabel.setFontScale(1.2f);
+        confirmLabel.setY(140);
+        confirmLabel.setAlignment(Align.center);
+        confirmLabel.setWidth(500);
+        confirmLabel.setHeight(120);
+        confirmLabel.setText("CONFIRM");
+        confirmLabel.setVisible(true);
+
+        BitmapFont infoBitmapFont = new BitmapFont(Gdx.files.internal("flat-earth/skin/fonts/pixel_32_info.fnt"));
+        Label.LabelStyle infostyle = new Label.LabelStyle();
+        infostyle.fontColor=new Color(66  , 39, 39, 1);
+        infostyle.font=infoBitmapFont;
+        infoLabel = new Label("If he is a traitor, you will succeed in saving Atlantis!\n\nOtherwise, keep looking for clues!", infostyle);
+        infoLabel.setFontScale(0.46f);
+        infoLabel.setY(10);
+        infoLabel.setAlignment(Align.center);
+        infoLabel.setWidth(500);
+        infoLabel.setHeight(220);
+        infoLabel.setText("If he is a traitor, you will succeed in saving Atlantis!\n\nOtherwise, keep looking for clues!");
+        infoLabel.setVisible(true);
+
+//        window.getTitleLabel().setStyle(style);
+//        window.getTitleLabel().setFontScale(1.5f);
+//        window.getTitleLabel().setY(-120);
+        window.setWidth(500);
+        window.setHeight(240);
+        // The default window position is in the lower left corner.
+        window.setX(Gdx.graphics.getWidth() / 2 - window.getWidth() /2 );
+        window.setY(Gdx.graphics.getHeight() / 2 - window.getHeight() / 2);
+        // Drag the TitleLabel, the window will move
+        window.setMovable(false);
+
+//        window.setBackground();
+        tbOk = new TextButton("OK", mskin);
+        tbCancel = new TextButton("CANCEL", mskin);
+        tbOk.setSize(tbCancel.getPrefWidth(), tbCancel.getPrefHeight());
+        tbCancel.addListener(new ClickListener() {
+
+            @Override
+            public void clicked(InputEvent event, float x, float y) {
+                window.setVisible(false);
+            }
+
+        });
+        tbOk.addListener(new ClickListener() {
+
+            @Override
+            public void clicked(InputEvent event, float x, float y) {
+                Gdx.app.log("TAG", "dialog ok button is clicked");
+                game.setScreen(GdxGame.ScreenType.MAIN_MENU);
+                window.setVisible(false);
+            }
+
+        });
+        tbOk.setX(window.getWidth() / 2+70);
+        tbOk.setY(20);
+        tbCancel.setX(window.getWidth() / 2-140);
+        tbCancel.setY(20);
+
+        window.addActor(infoLabel);
+        window.addActor(confirmLabel);
+        window.addActor(tbOk);
+        window.addActor(tbCancel);
+        window.setVisible(false);
+
+
         Label title = new Label("Npc Eviction Menu", skin, "title");
         Table menuNpcs = makeNpcCards();
         Table menuNpcs_two = makeNpcCards();
@@ -62,11 +142,17 @@ public class NpcEvictionMenuDisplay extends UIComponent {
 
         rootTable.add(exitBtn).expandY();
         rootTable.debug();
-
         stage.addActor(rootTable);
-
+        stage.addActor(window);
     }
+    ClickListener confirmListener= new ClickListener() {
 
+        @Override
+        public void clicked(InputEvent event, float x, float y) {
+            Gdx.app.log("TAG", "dialog ok button is clicked");
+            window.setVisible(true);
+        }
+    };
     private Table makeNpcCards() {
 
         TextButton confirmBtn1 = new TextButton("Confirm", skin);
@@ -81,15 +167,10 @@ public class NpcEvictionMenuDisplay extends UIComponent {
         ImageButton npcButton_three = new ImageButton(drawable);
         ImageButton npcButton_four = new ImageButton(drawable);
 
-        confirmBtn1.addListener(
-                new ChangeListener() {
-                    @Override
-                    public void changed(ChangeEvent changeEvent, Actor actor) {
-                        logger.debug("confirm button clicked");
-                       // confirmButtonCheck(); this function is used for check traitor
-                    }
-                });
-
+        confirmBtn1.addListener(confirmListener);
+        confirmBtn2.addListener(confirmListener);
+        confirmBtn3.addListener(confirmListener);
+        confirmBtn4.addListener(confirmListener);
         npcButton.addListener(
                 new ChangeListener() {
                     @Override
