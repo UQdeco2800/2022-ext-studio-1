a202f6de4cadc5bf0253454be825bd12ae64d21b
jeffjeffUQ <chihengjeffrey.hui@uqconnect.edu.au>
2022-08-31 04:24:13 +0800

    retry code change

--- source/core/src/main/com/deco2800/game/screens/MainGameScreen.java
+++ source/core/src/main/com/deco2800/game/screens/MainGameScreen.java
@@ -137,7 +137,7 @@ public class MainGameScreen extends ScreenAdapter {
         .addComponent(new PerformanceDisplay())
         .addComponent(new MainGameActions(this.game))
         .addComponent(new MainGameExitDisplay())
-       
+        .addComponent(new countdownDisplay(this.game))
         .addComponent(new Terminal())
         .addComponent(inputComponent)
         .addComponent(new TerminalDisplay());
