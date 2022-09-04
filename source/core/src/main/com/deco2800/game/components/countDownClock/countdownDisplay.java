1e0189d96735125810373c36f7b80453cb2188ce
LIU-Evelyn <lyxhahcjy@outlook.com>
2022-09-04 16:33:22 +0800

    Comment the countDownClock error

--- source/core/src/main/com/deco2800/game/components/countDownClock/countdownDisplay.java
+++ source/core/src/main/com/deco2800/game/components/countDownClock/countdownDisplay.java
@@ -41,15 +41,15 @@ public class countdownDisplay extends UIComponent {
     private void addActors() {
 
         Label.LabelStyle counterLabelStyle = new Label.LabelStyle();
-//        BitmapFont myFont = new BitmapFont(Gdx.files.internal());
-//        counterLabelStyle.font = myFont;
-        counterLabelStyle.fontColor = Color.YELLOW;
-
-        counterLabel = new Label(String.valueOf(timeRemaining), counterLabelStyle);
-        counterLabel.setPosition((float) (stage.getWidth() * 0.9), (float) (stage.getHeight() * 0.1));
-        counterLabel.setSize((float) (stage.getWidth() * 0.05), (float) (stage.getHeight() * 0.03));
-
-        stage.addActor(counterLabel);
+   //     BitmapFont myFont = new BitmapFont(Gdx.files.internal());
+     //    counterLabelStyle.font = myFont;
+//        counterLabelStyle.fontColor = Color.YELLOW;
+//
+//        counterLabel = new Label(String.valueOf(timeRemaining), counterLabelStyle);
+//        counterLabel.setPosition((float) (stage.getWidth() * 0.9), (float) (stage.getHeight() * 0.1));
+//        counterLabel.setSize((float) (stage.getWidth() * 0.05), (float) (stage.getHeight() * 0.03));
+//
+//        stage.addActor(counterLabel);
     }
 
     @Override
