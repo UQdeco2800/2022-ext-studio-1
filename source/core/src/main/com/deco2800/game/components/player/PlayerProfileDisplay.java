ab2570c8b68d82ef0bfc48b9269ccca8180cccb9
田辺　梨桜 <rio0618@outlook.com>
2022-09-25 15:10:27 +0900

    test window for button click

--- source/core/src/main/com/deco2800/game/components/player/PlayerProfileDisplay.java
+++ source/core/src/main/com/deco2800/game/components/player/PlayerProfileDisplay.java
@@ -2,8 +2,10 @@ package com.deco2800.game.components.player;
 
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.scenes.scene2d.Actor;
 import com.badlogic.gdx.scenes.scene2d.ui.Label;
 import com.badlogic.gdx.scenes.scene2d.ui.Table;
+import com.badlogic.gdx.scenes.scene2d.utils.ChangeListener;
 import com.deco2800.game.services.ServiceLocator;
 import com.deco2800.game.ui.UIComponent;
 import com.deco2800.game.services.ResourceService;
@@ -11,6 +13,7 @@ import org.slf4j.Logger;
 import com.badlogic.gdx.scenes.scene2d.ui.Window;
 import com.badlogic.gdx.scenes.scene2d.ui.Image;
 import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.scenes.scene2d.ui.TextButton;
 
 
 public class PlayerProfileDisplay extends UIComponent {
@@ -20,6 +23,8 @@ public class PlayerProfileDisplay extends UIComponent {
 
     Table root;
 
+    TextButton exitButton;
+
     @Override
     public void create() {
         super.create();
@@ -28,22 +33,42 @@ public class PlayerProfileDisplay extends UIComponent {
 
 
     private void addActors() {
-//        Image bgImage = new Image(
-//                ServiceLocator.getResourceService().getAsset(
-//                        "images/Blank.png", Texture.class
-//                )
-//        );
+        Image bgImage = new Image(
+                ServiceLocator.getResourceService().getAsset(
+                        "images/blank.png", Texture.class
+                )
+        );
+
+        exitButton = new TextButton("Exit", skin);
+
+        exitButton.addListener(
+                new ChangeListener() {
+                    @Override
+                    public void changed(ChangeEvent event, Actor actor) {
+                        closeWindow();
+                    }
+                }
+        );
+
+        exitButton.setPosition((float) (this.stage.getWidth() * 0.5), (float) (this.stage.getHeight() * 0.1));
+
 
         root = new Table();
         root.setFillParent(true);
-//        root.add(bgImage).height(Gdx.graphics.getHeight()-bgHeight).width(Gdx.graphics.getWidth()-bgWidth);
-        root.add(new Label("Player Profile", skin));
+        root.add(bgImage).height(Gdx.graphics.getHeight()-bgHeight).width(Gdx.graphics.getWidth()-bgWidth);
         this.stage.addActor(root);
+        this.stage.addActor(exitButton);
     }
 
     @Override
     public void draw(SpriteBatch batch)  {
         // draw is handled by the stage
+    }
+
+    private void closeWindow() {
+        super.dispose();
+        root.remove();
+        exitButton.remove();
     }
 
 }
