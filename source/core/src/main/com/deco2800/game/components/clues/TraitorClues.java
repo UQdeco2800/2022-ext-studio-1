9b7e5eaba020501035bcbb3e809a8dfe5404dfd3
WenjiWangkang <w.wangkang@uq.net.au>
2022-10-04 22:58:52 +1000

    Added some more clues for NPCClueLibrary and create two new clueLibraries.

---
+++ source/core/src/main/com/deco2800/game/components/clues/TraitorClues.java
@@ -0,0 +1,12 @@
+package com.deco2800.game.components.clues;
+
+import java.util.ArrayList;
+
+public class TraitorClues extends ClueLibrary{
+
+    public TraitorClues(ArrayList<String> traitorClues){
+        traitorClues.add("A man who has a deep voice.");
+        traitorClues.add(" A tall, thin person.");
+        traitorClues.add("A man who was close to Nereus.");
+    }
+}
