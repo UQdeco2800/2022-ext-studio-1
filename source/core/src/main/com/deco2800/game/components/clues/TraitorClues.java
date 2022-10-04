dedba3006bc80592581948df30084b38762ceff6
WenjiWangkang <w.wangkang@uq.net.au>
2022-10-04 23:49:18 +1000

    Added a comment

--- source/core/src/main/com/deco2800/game/components/clues/TraitorClues.java
+++ source/core/src/main/com/deco2800/game/components/clues/TraitorClues.java
@@ -1,7 +1,9 @@
 package com.deco2800.game.components.clues;
 
 import java.util.ArrayList;
-
+/**
+ * Singleton class for Traitor Clue Library
+ */
 public class TraitorClues extends ClueLibrary{
 
     public TraitorClues(ArrayList<String> traitorClues){
