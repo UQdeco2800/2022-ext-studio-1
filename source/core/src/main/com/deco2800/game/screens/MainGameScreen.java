1412964e78b4e478b6c0538716688924555ef493
jeffjeffUQ <chihengjeffrey.hui@uqconnect.edu.au>
2022-10-18 01:03:45 +0800

    setup for game over transition

--- source/core/src/main/com/deco2800/game/screens/MainGameScreen.java
+++ source/core/src/main/com/deco2800/game/screens/MainGameScreen.java
@@ -86,14 +86,14 @@ public class MainGameScreen extends ScreenAdapter {
 
   private boolean stopGame;
 
- // private  float gameDuration;
+  private  float gameDuration;
 
   private static final String backgroundMusic = "sounds/new.mp3";
 
-  public MainGameScreen(GdxGame game, boolean stop) {
+  public MainGameScreen(GdxGame game, boolean stop,float gameTime) {
     this.game = game;
     this.stopGame = stop;
-    //this.gameDuration =gameTime;
+    this.gameDuration =gameTime;
 
     logger.debug("Initialising main game screen services");
     ServiceLocator.registerTimeSource(new GameTime());
@@ -147,10 +147,10 @@ public class MainGameScreen extends ScreenAdapter {
 
 
     }
-    //if(game.gameTimeVar==0){
+    if(game.gameTimeVar==0){
       //System.out.println("Testing:Game is over");
 
-    //}
+    }
   }
 
   @Override
