9406b1662f065f691d46fbd3018de0440376933a
Ty Briggs <tycbriggs@gmail.com>
2022-10-18 20:45:55 +1000

    Added damage flash

--- source/core/src/main/com/deco2800/game/areas/ForestGameArea.java
+++ source/core/src/main/com/deco2800/game/areas/ForestGameArea.java
@@ -7,6 +7,7 @@ import com.deco2800.game.GdxGame;
 import com.deco2800.game.areas.terrain.TerrainFactory;
 import com.deco2800.game.areas.terrain.TerrainFactory.TerrainType;
 import com.deco2800.game.components.gameoverScreen.GameOverDisplay;
+import com.deco2800.game.components.player.DamageFlashDisplayComponent;
 import com.deco2800.game.entities.Entity;
 import com.deco2800.game.entities.factories.*;
 import com.deco2800.game.utils.math.GridPoint2Utils;
@@ -25,6 +26,8 @@ import java.util.List;
  */
 public class ForestGameArea extends GameArea {
     private static final Logger logger = LoggerFactory.getLogger(ForestGameArea.class);
+
+    private DamageFlashDisplayComponent damageFlash;
     private static final int NUM_TREES = 7;
     private static final int NUM_GHOSTS = 2;
     private static final int NUM_BATTERIES = 3;
@@ -90,11 +93,13 @@ public class ForestGameArea extends GameArea {
             "images/switch/Battery.png",
             "images/switch/Electric Switch Broken.png",
             "images/KEY.png",
+            "images/damageFlash.png",
             "images/coral/scales1.png",
             "images/coral/scales2.png",
             "images/coral/scales3.png",
             "images/coral/scales4.png",
             "images/coral/scales5.png",
+
     };
     private static final String[] forestTextureAtlases = {
             "images/terrain_iso_grass.atlas", "images/ghost.atlas", "images/ghostKing.atlas", "images/player.atlas", "images/orpheus.atlas",
@@ -327,12 +332,14 @@ public class ForestGameArea extends GameArea {
         return newPlayer;
     }
     public void updatePlayerHealth(int health) {
+        damageFlash = new DamageFlashDisplayComponent();
         if (health <= 0)
         {
             logger.debug("Game Over: Lost Health");
             game.theGameScreen.changeStatus();
             new GameOverDisplay(this.game).create();
         }
+        damageFlash.waitTime();
     }
     public void spawnTimeConsumeableItem() {
 //    Entity item = ConsumableItemFactory.createItem(player, "images/inventory/time_item.png");
