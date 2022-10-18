7f30b6c04c6bf89ddc50614c155132ab857364f1
jeffjeffUQ <chihengjeffrey.hui@uqconnect.edu.au>
2022-10-18 20:30:42 +0800

    comment out for testing

--- source/core/src/main/com/deco2800/game/components/npcEvictionMenu/NpcEvictionMenuDisplayNew.java
+++ source/core/src/main/com/deco2800/game/components/npcEvictionMenu/NpcEvictionMenuDisplayNew.java
@@ -206,7 +206,7 @@ public class NpcEvictionMenuDisplayNew extends UIComponent {
             //file[playerStats].writeString(profileData,true);
 
             //Use the setter in MainGameScreen.java, so that it can change the checkGameResultVariable,which will help display the right ending menu when ran out of time
-            game.theGameScreen.setterForGameResult();
+            //game.theGameScreen.setterForGameResult();
 
             errorNum = 0;
             return NpcResultDialogType.RIGHT_BOX;
